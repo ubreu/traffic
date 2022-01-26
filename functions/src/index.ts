847e3469a7ceda9eebc4b1f595c4f0025945b989
Urs Breu <urs.breu@ergon.ch>
2022-01-26 18:20:52 +0100

    apply lint suggestions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,19 @@
 import * as functions from "firebase-functions";
-const https = require('https');
-const cors = require('cors')({
-   origin: true,
- });
+import https = require("https");
+import c = require("cors");
+const cors = c({origin: true});
 
-const URL = "https://trafficmapsrgssr.trafficintelligence.ch/api/event/GetEventsTrafficApi/46.27634554693529,5.704062499999999,47.120753018933755,12.3009375/11,12,13,14,90/1/5/2"
+const URL = "https://trafficmapsrgssr.trafficintelligence.ch/api/event/GetEventsTrafficApi/46.27634554693529,5.704062499999999,47.120753018933755,12.3009375/11,12,13,14,90/1/5/2";
 
 export const trafficInfo = functions.https.onRequest((request, response) => {
-   cors(request, response, () => {
-      let data = '';
-      https.get(URL, (res: any) => {
-            res.on('data', (d: any) => {
-                data += d;
-            });
-            res.on('end', () => response.status(200).send(data));
-            res.on('error', () => response.sendStatus(400));  
-        });
+  cors(request, response, () => {
+    let data = "";
+    https.get(URL, (res: any) => {
+      res.on("data", (d: any) => {
+        data += d;
+      });
+      res.on("end", () => response.status(200).send(data));
+      res.on("error", () => response.sendStatus(400));
     });
+  });
 });
