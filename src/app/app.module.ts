31a77edf005b518bec64bbcad72372421f194ca0
Urs Breu <urs.breu@ergon.ch>
2022-02-02 22:22:10 +0100

    add search form for quickly finding an entry
    - improve observable handling by sharing async pipe
    - do not call API when filtering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireFunctionsModule, USE_EMULATOR as USE_FUNCTIONS_EMULATOR } from '@angular/fire/compat/functions';
@@ -7,10 +8,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OverviewComponent } from './overview/overview.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 
 import { environment } from '../environments/environment';
@@ -30,12 +34,17 @@ import { EntryListComponent } from './entry-list/entry-list.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     HttpClientModule,
+    MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: USE_FUNCTIONS_EMULATOR, useValue: environment.useEmulators ? ['localhost', 5001] : undefined },
--- src/app/entry-list/entry-list.component.ts
+++ src/app/entry-list/entry-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router'
 import { Observable } from 'rxjs'
-import { map, tap } from 'rxjs/operators'
+import { combineLatestWith, debounceTime, distinctUntilChanged, map, tap, startWith } from 'rxjs/operators'
 import { TrafficEntity } from '../domain/traffic-info'
 import { Category } from '../domain/traffic-info'
 import { NavigationPath, RouteParam } from '../infra/path';
@@ -14,21 +15,34 @@ import { TrafficService } from '../services/traffic.service'
 })
 export class EntryListComponent implements OnInit {
 
-  entities$!: Observable<TrafficEntity[]>
-  category: Category = Category.CONDITION
-  loading = true
 
+  availableNames: string[] = [""]
+  category: Category = Category.CONDITION
+  filteredEntities$!: Observable<TrafficEntity[]>
+  entities$!: Observable<TrafficEntity[]>
+  searchControl = new FormControl();
   constructor(private router: Router, private route: ActivatedRoute, private trafficService: TrafficService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.category = params.get(RouteParam.CATEGORY) as Category
       this.entities$ = this.trafficService.getTrafficInfo(this.category).pipe(
-        tap(i => this.loading = false),
         map(i => i.Entity),
         tap(results => results.sort((x, y) => x.Name < y.Name ? -1 : 1))
       )
+
+      this.filteredEntities$ = this.searchControl.valueChanges.pipe(
+        startWith(''),
+        debounceTime(300),
+        distinctUntilChanged(),
+        combineLatestWith(this.entities$),
+        map(([filterValue, e]) => this.filter(filterValue, e)))
     })
+  }
+
+  private filter(value: string, entities: TrafficEntity[]): TrafficEntity[] {
+    const filterValue = value.toLowerCase();
+    return entities.filter(e => e.Name.toLowerCase().includes(filterValue))
   }
 
   backToOverview(): void {
